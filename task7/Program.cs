f22368e8492570a190837686ade0f53122f34404
Anna <anya-suenkova@yandex.ru>
2022-12-17 22:36:19 +0300

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 100); 
+Console.Write("Произвольное число ");
+Console.WriteLine(number);
+
+int a = number % 10;
+int b = number / 10;
+
+if (a > b)
+{
+    Console.Write("Наибольшая цифра числа: ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Наибольшая цифра числа: ");
+    Console.WriteLine(b);
+}
