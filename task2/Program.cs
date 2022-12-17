8aa12836231bc0596e023274409ed3f6e29d8529
Anna <anya-suenkova@yandex.ru>
2022-12-17 16:13:11 +0300

    add Task2

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Enter any number from 1 to 7 : ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number == 1)
+{
+   Console.Write("Monday");
+}
+if (number == 2)
+{
+   Console.Write("Tuesday");
+}
+if (number == 3)
+{
+    Console.Write("Wednesday");
+}
+if (number == 4)
+{
+    Console.Write("Thursday");
+}
+if (number == 5)
+{
+    Console.Write("Friday");
+}
+if (number == 6)
+{
+    Console.Write("Saturday");
+}
+if (number == 7)
+{
+    Console.Write("Sunday");
+}
