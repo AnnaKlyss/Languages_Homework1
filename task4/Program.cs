6a0d2d868af3061540fcde4c35ff8db2b7b388fe
Anna <anya-suenkova@yandex.ru>
2022-12-17 17:36:22 +0300

    Task4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if ( a % 2 == 0)
+{ 
+   Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
+    
