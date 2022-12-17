5271fc5886fb25cf1570bf496705bd77cf13678b
Anna <anya-suenkova@yandex.ru>
2022-12-17 15:56:26 +0300

    add .gitignore and task1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число В: ");
+int b = int.Parse(Console.ReadLine()!);
+int c = b*b;
+if (a == c)
+{ 
+   Console.WriteLine("Число А является квадратом числа В");
+}   
+else
+{
+    Console.WriteLine("Число А не является квадратом числа В");
+}
