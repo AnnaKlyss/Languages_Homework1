ad6ba89b87eb4646fc491c3b60a98dbb4e8bd692
Anna <anya-suenkova@yandex.ru>
2022-12-17 23:22:01 +0300

    task9

---
+++ task9/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите число N = ");
+double n = double.Parse(Console.ReadLine()!);
+Console.Write("Введите число A = ");
+double a = double.Parse(Console.ReadLine()!);
+
+if ( n % a == 0)
+{ 
+   Console.WriteLine("Число N кратно заданному числу А");
+}
+else
+{
+    double b =  n % a;
+    Console.Write("Остаток от деления равен: ");
+    Console.WriteLine(b);
+}
