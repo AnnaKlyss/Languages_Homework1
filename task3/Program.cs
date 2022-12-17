5618b53f1b19de7914a4e382f0fb6de1126c56d8
Anna <anya-suenkova@yandex.ru>
2022-12-17 17:05:07 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Enter number А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number В: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number C: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{ 
+    if (a > c)
+    {
+     Console.Write("Max number A = ");
+     Console.WriteLine(a);
+    }
+   else
+   {
+    Console.Write("Max number C = ");
+    Console.WriteLine(c);
+   }
+}
+else
+{
+  if (b > c)
+     {
+     Console.Write("Max number B = ");
+     Console.WriteLine(b);
+    }
+   else
+   {
+    Console.Write("Max number C = ");
+    Console.WriteLine(c);
+   }
+}
