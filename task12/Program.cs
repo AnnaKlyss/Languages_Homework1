0d4cbf903e98c126bdf1e47a5075df887b7a536f
Anna <anya-suenkova@yandex.ru>
2022-12-19 18:13:16 +0300

    add Task12

---
+++ task12/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую 
+//  день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Enter any number from 1 to 7 : ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number <= 5)
+{
+   Console.Write("No");
+}
+else
+{
+    Console.Write("Yes");
+}
