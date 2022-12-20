5a92fe0a5aaa59628a00b11f4c17f6847bd3582c
Anna <anya-suenkova@yandex.ru>
2022-12-20 23:28:10 +0300

    task15

--- task15(23)/Program.cs
+++ task15(23)/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Введите значениe N: ");
+double N = double.Parse(Console.ReadLine()!);
+
+double i = 1;
+do
+{
+    double y = Math.Pow(i, 3);
+    Console.Write(y + ", ");
+    i++;
+} while (i <= N);
