f8f74bbfaa3e8e302948f390f025e4485a5c8e6f
Anna <anya-suenkova@yandex.ru>
2022-12-21 23:48:11 +0300

    task17

--- task17(27)/Program.cs
+++ task17(27)/Program.cs
@@ -3,3 +3,12 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.WriteLine(sum);
