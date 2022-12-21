4dbbcd3ba8a76d22050c9663ad5d0539b478152e
Anna <anya-suenkova@yandex.ru>
2022-12-21 18:27:54 +0300

    task16

--- task16(25)/Program.cs
+++ task16(25)/Program.cs
@@ -3,3 +3,19 @@
 // Не используя Math.Pow!
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите значение A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (b < 0 )
+{
+    b = b * (-1);
+}
+int result = 1;
+for (int i = 1; i <= b;  i++)
+{
+    result = result * a;
+}
+Console.WriteLine("Число " + a + " в степени " + b + " равно " + result);
