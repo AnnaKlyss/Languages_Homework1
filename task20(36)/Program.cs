b4786854701f3656463184ea81a7fc957705c249
Anna <anya-suenkova@yandex.ru>
2022-12-23 22:14:52 +0300

    task20

---
+++ task20(36)/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10]; 
+
+for (int i = 0; i < array.Length; i++) 
+{
+    int randomNumber = new Random().Next(-100, 101); 
+    array[i] = randomNumber; 
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях.: " + sum);
